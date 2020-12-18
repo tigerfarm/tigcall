97acaee81376d5aedf51b45f36f21a8b806d5bec
tigerfarm <tigerfarm@gmail.com>
2020-12-18 12:13:20 -0800

    updates

--- tigcall/accountNumberList.php
+++ tigcall/accountNumberList.php
@@ -10,7 +10,7 @@ if ($tokenPassword == "") {
 }
 $token_password = getenv("TOKEN_PASSWORD");
 if ($token_password !== $tokenPassword) {
-    echo "0" . " Environment:" . $token_password . ": Parameter:" . $tokenPassword . ":";
+    // echo "0" . " Environment:" . $token_password . ": Parameter:" . $tokenPassword . ":";
     echo "0";
     return;
 }
